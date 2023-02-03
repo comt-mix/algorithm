a059ead9bd2bb63019b530b009c44306da2543c8
comt.mix@gmail.com <comt.mix@gmail.com>
2023-02-03 19:17:01 +0900

    Feat: day17 TIL

---
+++ Grind 169 questions/Week 3/Implement Trie (Prefix Tree)/Implement Trie (Prefix Tree).js
@@ -0,0 +1,64 @@
+/**
+ * 처음 풀었던 풀이법
+ */
+
+/**
+ * 참고한 풀이법
+ * 개인적으로 이 풀이법 보면서 감탄함. 너무 좋은 풀이법.
+ */
+const Trie = function () {
+  this.root = {};
+};
+
+/**
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function (word) {
+  let node = this.root;
+
+  for (const x of word) {
+    if (!node[x]) node[x] = {};
+    node = node[x];
+  }
+
+  node.isEnd = true;
+};
+
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function (word) {
+  let node = this.root;
+
+  for (let x of word) {
+    if (!node[x]) return false;
+    node = node[x];
+  }
+
+  return node.isEnd === true;
+};
+
+/**
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function (prefix) {
+  let node = this.root;
+
+  for (let x of prefix) {
+    if (!node[x]) return false;
+    node = node[x];
+  }
+
+  return true;
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
