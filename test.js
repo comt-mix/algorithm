328029be50fa66b9d7f98ec66a7709a38c32d5af
comt.mix@gmail.com <comt.mix@gmail.com>
2023-02-27 22:09:28 +0900

    Feat: TIL(27)

---
+++ test.js
@@ -0,0 +1,3 @@
+function solution(A) { let max = Number.MIN_SAFE_INTEGER; const array =
+Array.from({ length: A.length }, () => false); console.log(array); } solution([
+[15, 1, 5], [16, 3, 8], [2, 6, 4], ]);
