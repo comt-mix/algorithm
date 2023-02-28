6427fcb46bfb3a14cbd6392b96ca30c292569266
comt.mix@gmail.com <comt.mix@gmail.com>
2023-02-28 16:15:14 +0900

    Feat: TIL(28)

---
+++ Grind 169 questions/Week5/Majority Element/Majority Element.js
@@ -0,0 +1,25 @@
+/**
+ * 처음 풀었던 풀이법
+ * testcases failed
+ */
+
+const majorityElement = (nums) => {
+  const map = new Map();
+
+  for (const x of nums) {
+    if (!map.has(x)) map.set(x, 1);
+    else map.set(x, map.get(x) + 1);
+  }
+
+  let max = 0;
+  let key = 0;
+
+  for (const keys of map.keys()) {
+    if (max < map.get(keys)) {
+      max = map.get(keys);
+      key = keys;
+    }
+  }
+
+  return key;
+};
